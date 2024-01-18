6fef4fb8728773450d807ec71a55aa33d614689b
Maria Carrizo <maria3carrizo@gmail.com>
2024-01-18 07:54:38 +0100

    change in resistive layer to 0.03 S/m

--- Synth-3Layers/B2/2_CreateSyntheticData_B2.py
+++ Synth-3Layers/B2/2_CreateSyntheticData_B2.py
@@ -22,7 +22,7 @@ nlayer = 3 # number of layer
 npos = 20 # number of sampling positions
 
 # 3 layered conductivities
-sigmas = [1/5, 1/50, 1/5] 
+sigmas = [1/5, 0.03, 1/5] 
 sigmas = np.ones((npos, nlayer))*sigmas
 x = np.linspace(0, 20, npos)[:,None]
 
