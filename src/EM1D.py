58eb2f7c3b9e18fe0edd9f47c89efa5d7740f186
Maria <carrizomascarell@terrasysgeo.com>
2024-04-03 15:15:33 +0200

    added function EMf_2Lay_HP_field

--- src/EM1D.py
+++ src/EM1D.py
@@ -455,6 +455,37 @@ def EMf_3Lay_HV_field(lambd, sigma1, sigma2, sigma3, h1, h2, height, offsets, fr
     IP_v = np.abs(Z_v.real[1:])
     return np.hstack((Q_h, Q_v, IP_h, IP_v))
 
+def EMf_3Lay_HP_field(lambd, sigma1, sigma2, sigma3, h1, h2, height, offsets, freq, filt):
+    """ Forward function for a 3-layered earth model
+        Input:
+        lam : radial component of the wavenumber
+        sigma1 : electrical conductivity of 1st layer in S/m
+        sigma2 : electrical conductivity of 2nd layer in S/m
+        sigma3 : electrical conductivity of 3rd layer in S/m
+        h1 : thickness of 1st layer in m
+        h2 : thickness of 2nd layer in m
+        height : height of the instrument above ground in m
+        offsets : coil separation in m
+        freq : frequency in Hertz
+        filt : filter to perform the hankel transform
+        Output:
+        data : vector with the Quadrature (Q) and In-phase (IP) components of the
+        measurements for the H, V and P coil orientations as
+        [Q_H, IP_H, Q_P, IP_P]
+    """
+    # Calculate reflection coefficient
+    R0 = R0_3Lay(lambd, sigma1, sigma2, sigma3, h1, h2, freq)
+    # Calculate mutual impedance ratios for each coil-coil geometry
+    Z_h = Z_H(s=offsets, R_0= R0, lambd=lambd, a=height, filt=filt)
+    Z_p = Z_P(s=offsets, R_0= R0, lambd=lambd, a=height, filt=filt)
+    # Obtain quadratures
+    Q_h = np.abs(Z_h.imag)
+    Q_p = np.abs(Z_p.imag)
+    # Obtain in-phases
+    IP_h = np.abs(Z_h.real)
+    IP_p = np.abs(Z_p.real)
+    return np.hstack((Q_h, IP_h, Q_p, IP_p))
+
 def EMf_3Lay_HVP_Q(lambd, sigma1, sigma2, sigma3, h1, h2, height, offsets, freq, filt):
     """ Forward function for a 2-layered earth model
     
