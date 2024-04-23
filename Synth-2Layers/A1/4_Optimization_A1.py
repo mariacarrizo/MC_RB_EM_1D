f5e967adbf8963221899f5b6034cb95253cd5f9b
Maria <carrizomascarell@terrasysgeo.com>
2024-04-23 17:13:25 +0200

    new forward modeller

--- Synth-2Layers/A1/4_Optimization_A1.py
+++ Synth-2Layers/A1/4_Optimization_A1.py
@@ -7,18 +7,25 @@ import sys
 sys.path.insert(1, '../../src')
 
 # Import forward modelling classes for 2-layered models
-from EM1D import EMf_2Lay_Opt_HVP, EMf_2Lay_Opt_HVP_Q, EMf_2Lay_Opt_HVP_IP
+from EM1D import EMf_2Lay_Opt_HVP_1D, EMf_2Lay_Opt_HVP_Q, EMf_2Lay_Opt_HVP_IP
 
 # Import the conductivities and thicknesses used to create the LU table
 conds = np.load('../data/conds.npy')
 thick = np.load('../data/thicks.npy')
 
 # Import true models and data 
-model = np.load('data/model_synth_2Lay_A1.npy')
-data = np.load('data/data_synth_2Lay_A1.npy')
+model_A1_1 = np.load('data/model_synth_2Lay_A1_1.npy')
+model_A1_2 = np.load('data/model_synth_2Lay_A1_2.npy')
+model_A1_3 = np.load('data/model_synth_2Lay_A1_3.npy')
+model_A1_4 = np.load('data/model_synth_2Lay_A1_4.npy')
+
+data_A1_1 = np.load('data/data_synth_2Lay_A1_1.npy')
+data_A1_2 = np.load('data/data_synth_2Lay_A1_2.npy')
+data_A1_3 = np.load('data/data_synth_2Lay_A1_3.npy')
+data_A1_4 = np.load('data/data_synth_2Lay_A1_4.npy')
 
 # Number of 1D models
-npos = len(data) # number of positions
+npos = len(data_A1_1) # number of positions
 
 # Load survey parameters
 survey = np.load('../data/survey_2Lay.npy', allow_pickle=True).item()
@@ -31,24 +38,53 @@ filt = survey['filt']
 #%%
 # Optimization Q + IP
 
+print('Estimating model A1-1 using Q+IP')
 # Initialize the forward modelling class 
 EMf = EMf_2Lay_Opt_HVP(lambd, height, offsets, freq, filt)
+
+# Define transformation
+transModel = pg.trans.TransLogLU(0.01,7) 
 
 # Define inversion framework from pygimli
 invEM = pg.Inversion()
 invEM.setForwardOperator(EMf) # set forward operator
+invEM.modelTrans = transModel
 
 # Relative error array
 error = 1e-3 # relative error
 relativeError = np.ones_like(data[0]) * error
-model_Opt = np.zeros_like(model)
+model_Opt_A1_1 = np.zeros_like(model_A1_1)
 
 # Start inversion
 # Perform inversion for each 1D model 
 for pos in range(npos):
-    dataE = data[pos].copy()
+    dataE = data_A1_1[pos].copy()
     model_Opt_pos = invEM.run(dataE, relativeError, verbose=False)
-    model_Opt[pos] = model_Opt_pos
+    model_Opt_A1_1[pos] = model_Opt_pos
+    
+print('Estimating model A1-2 using Q+IP')
+# Initialize the forward modelling class 
+EMf = EMf_2Lay_Opt_HVP(lambd, height, offsets, freq, filt)
+
+# Define transformation
+transModel = pg.trans.TransLogLU(0.01,7) 
+
+# Define inversion framework from pygimli
+invEM = pg.Inversion()
+invEM.setForwardOperator(EMf) # set forward operator
+invEM.modelTrans = transModel
+
+# Relative error array
+error = 1e-3 # relative error
+relativeError = np.ones_like(data[0]) * error
+model_Opt_A1_2 = np.zeros_like(model_A1_2)
+
+# Start inversion
+# Perform inversion for each 1D model 
+for pos in range(npos):
+    dataE = data_A1_2[pos].copy()
+    model_Opt_pos = invEM.run(dataE, relativeError, verbose=False)
+    model_Opt_A1_2[pos] = model_Opt_pos
 
 #%%
 # Optimization Q 
