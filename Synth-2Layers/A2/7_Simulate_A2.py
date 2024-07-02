099299f892529160c9f46e869bf13493fdcf7d2f
Maria <carrizomascarell@terrasysgeo.com>
2024-07-02 17:38:46 +0200

    changed script name

--- Synth-2Layers/A2/7_Simulate_A2.py
+++ Synth-2Layers/A2/7_Simulate_A2.py
@@ -1,4 +1,12 @@
-# Simulate data from estimated models
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" 
+Script Name: 8_Simulate_A1.py
+Description: Script to Simulate data from A2 estimated models
+Author: @mariacarrizo
+Email: m.e.carrizomascarell@tudelft.nl
+Date created: 19/12/2023
+"""
 
 # Import libraries
 import numpy as np
@@ -18,41 +26,41 @@ filt = survey['filt']
 
 # Load models
 
-# Results from global search
-model_GS_A2_1 = np.load('results/model_2Lay_A2_1.npy')
-model_GS_A2_2 = np.load('results/model_2Lay_A2_2.npy')
-model_GS_A2_3 = np.load('results/model_2Lay_A2_3.npy')
-model_GS_A2_4 = np.load('results/model_2Lay_A2_4.npy')
+# Results from global search Q + IP
+model_GS_A2_1 = np.load('results/model_GS_A2_1.npy')
+model_GS_A2_2 = np.load('results/model_GS_A2_2.npy')
+model_GS_A2_3 = np.load('results/model_GS_A2_3.npy')
+model_GS_A2_4 = np.load('results/model_GS_A2_4.npy')
 
-# Results from optimization
-model_Opt_A2_1 = np.load('results/model_Opt_A2_1.npy')
-model_Opt_A2_2 = np.load('results/model_Opt_A2_2.npy')
-model_Opt_A2_3 = np.load('results/model_Opt_A2_3.npy')
-model_Opt_A2_4 = np.load('results/model_Opt_A2_4.npy')
+# Results from Gauss-Newton Q + IP
+model_GN_A2_1 = np.load('results/model_GN_A2_1.npy')
+model_GN_A2_2 = np.load('results/model_GN_A2_2.npy')
+model_GN_A2_3 = np.load('results/model_GN_A2_3.npy')
+model_GN_A2_4 = np.load('results/model_GN_A2_4.npy')
 
 # Results from global search Q
-model_GS_Q_A2_1 = np.load('results/model_2Lay_Q_A2_1.npy')
-model_GS_Q_A2_2 = np.load('results/model_2Lay_Q_A2_2.npy')
-model_GS_Q_A2_3 = np.load('results/model_2Lay_Q_A2_3.npy')
-model_GS_Q_A2_4 = np.load('results/model_2Lay_Q_A2_4.npy')
+model_GS_Q_A2_1 = np.load('results/model_GS_Q_A2_1.npy')
+model_GS_Q_A2_2 = np.load('results/model_GS_Q_A2_2.npy')
+model_GS_Q_A2_3 = np.load('results/model_GS_Q_A2_3.npy')
+model_GS_Q_A2_4 = np.load('results/model_GS_Q_A2_4.npy')
 
-# Results from optimization Q
-model_Opt_Q_A2_1 = np.load('results/model_Opt_Q_A2_1.npy')
-model_Opt_Q_A2_2 = np.load('results/model_Opt_Q_A2_2.npy')
-model_Opt_Q_A2_3 = np.load('results/model_Opt_Q_A2_3.npy')
-model_Opt_Q_A2_4 = np.load('results/model_Opt_Q_A2_4.npy')
+# Results from Gauss-Newton Q
+model_GN_Q_A2_1 = np.load('results/model_GN_Q_A2_1.npy')
+model_GN_Q_A2_2 = np.load('results/model_GN_Q_A2_2.npy')
+model_GN_Q_A2_3 = np.load('results/model_GN_Q_A2_3.npy')
+model_GN_Q_A2_4 = np.load('results/model_GN_Q_A2_4.npy')
 
 # Results from global search IP
-model_GS_IP_A2_1 = np.load('results/model_2Lay_IP_A2_1.npy')
-model_GS_IP_A2_2 = np.load('results/model_2Lay_IP_A2_2.npy')
-model_GS_IP_A2_3 = np.load('results/model_2Lay_IP_A2_3.npy')
-model_GS_IP_A2_4 = np.load('results/model_2Lay_IP_A2_4.npy')
+model_GS_IP_A2_1 = np.load('results/model_GS_IP_A2_1.npy')
+model_GS_IP_A2_2 = np.load('results/model_GS_IP_A2_2.npy')
+model_GS_IP_A2_3 = np.load('results/model_GS_IP_A2_3.npy')
+model_GS_IP_A2_4 = np.load('results/model_GS_IP_A2_4.npy')
 
-# Results from optimization IP
-model_Opt_IP_A2_1 = np.load('results/model_Opt_IP_A2_1.npy')
-model_Opt_IP_A2_2 = np.load('results/model_Opt_IP_A2_2.npy')
-model_Opt_IP_A2_3 = np.load('results/model_Opt_IP_A2_3.npy')
-model_Opt_IP_A2_4 = np.load('results/model_Opt_IP_A2_4.npy')
+# Results from Gauss-Newton IP
+model_GN_IP_A2_1 = np.load('results/model_GN_IP_A2_1.npy')
+model_GN_IP_A2_2 = np.load('results/model_GN_IP_A2_2.npy')
+model_GN_IP_A2_3 = np.load('results/model_GN_IP_A2_3.npy')
+model_GN_IP_A2_4 = np.load('results/model_GN_IP_A2_4.npy')
 
 # Create empty array for true data in each position
 data_GS_A2_1 = []
@@ -70,24 +78,27 @@ data_GS_IP_A2_2 = []
 data_GS_IP_A2_3 = []
 data_GS_IP_A2_4 = []
 
-data_Opt_A2_1 = []
-data_Opt_A2_2 = []
-data_Opt_A2_3 = []
-data_Opt_A2_4 = []
+data_GN_A2_1 = []
+data_GN_A2_2 = []
+data_GN_A2_3 = []
+data_GN_A2_4 = []
 
-data_Opt_Q_A2_1 = []
-data_Opt_Q_A2_2 = []
-data_Opt_Q_A2_3 = []
-data_Opt_Q_A2_4 = []
+data_GN_Q_A2_1 = []
+data_GN_Q_A2_2 = []
+data_GN_Q_A2_3 = []
+data_GN_Q_A2_4 = []
 
-data_Opt_IP_A2_1 = []
-data_Opt_IP_A2_2 = []
-data_Opt_IP_A2_3 = []
-data_Opt_IP_A2_4 = []
+data_GN_IP_A2_1 = []
+data_GN_IP_A2_2 = []
+data_GN_IP_A2_3 = []
+data_GN_IP_A2_4 = []
 
+# Number of 1D model positions
 npos = len(model_GS_A2_1)
 
-# Simulate data in each position
+# Simulate data in each position 
+
+# Global search Q + IP
 for i in range(npos):
     data_GS_A2_1.append(EMf_2Lay_HVP(lambd, 
                                      sigma1 = model_GS_A2_1[i,1], 
@@ -125,6 +136,7 @@ for i in range(npos):
                                      freq = freq,
                                      filt = filt))
 
+# Global search Q 
 for i in range(npos):
     data_GS_Q_A2_1.append(EMf_2Lay_HVP_Q(lambd, 
                                      sigma1 = model_GS_Q_A2_1[i,1], 
@@ -161,7 +173,8 @@ for i in range(npos):
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt))
-    
+
+# Global search IP
 for i in range(npos):
     data_GS_IP_A2_1.append(EMf_2Lay_HVP_IP(lambd, 
                                      sigma1 = model_GS_IP_A2_1[i,1], 
@@ -199,120 +212,122 @@ for i in range(npos):
                                      freq = freq,
                                      filt = filt))
     
-# Simulate data in each position
+# Gauss-Newton Q + IP
 for i in range(npos):
-    data_Opt_A2_1.append(EMf_2Lay_HVP(lambd, 
-                                     sigma1 = model_Opt_A2_1[i,1], 
-                                     sigma2 = model_Opt_A2_1[i,2], 
-                                     h1 = model_Opt_A2_1[i,0], 
+    data_GN_A2_1.append(EMf_2Lay_HVP(lambd, 
+                                     sigma1 = model_GN_A2_1[i,1], 
+                                     sigma2 = model_GN_A2_1[i,2], 
+                                     h1 = model_GN_A2_1[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt)) 
     
-    data_Opt_A2_2.append(EMf_2Lay_HVP(lambd, 
-                                     sigma1 = model_Opt_A2_2[i,1], 
-                                     sigma2 = model_Opt_A2_2[i,2], 
-                                     h1 = model_Opt_A2_2[i,0], 
+    data_GN_A2_2.append(EMf_2Lay_HVP(lambd, 
+                                     sigma1 = model_GN_A2_2[i,1], 
+                                     sigma2 = model_GN_A2_2[i,2], 
+                                     h1 = model_GN_A2_2[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt)) 
     
-    data_Opt_A2_3.append(EMf_2Lay_HVP(lambd, 
-                                     sigma1 = model_Opt_A2_3[i,1], 
-                                     sigma2 = model_Opt_A2_3[i,2], 
-                                     h1 = model_Opt_A2_3[i,0], 
+    data_GN_A2_3.append(EMf_2Lay_HVP(lambd, 
+                                     sigma1 = model_GN_A2_3[i,1], 
+                                     sigma2 = model_GN_A2_3[i,2], 
+                                     h1 = model_GN_A2_3[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt))
 
-    data_Opt_A2_4.append(EMf_2Lay_HVP(lambd, 
-                                     sigma1 = model_Opt_A2_4[i,1], 
-                                     sigma2 = model_Opt_A2_4[i,2], 
-                                     h1 = model_Opt_A2_4[i,0], 
+    data_GN_A2_4.append(EMf_2Lay_HVP(lambd, 
+                                     sigma1 = model_GN_A2_4[i,1], 
+                                     sigma2 = model_GN_A2_4[i,2], 
+                                     h1 = model_GN_A2_4[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt))
 
+# Gauss-Newton Q
 for i in range(npos):
-    data_Opt_Q_A2_1.append(EMf_2Lay_HVP_Q(lambd, 
-                                     sigma1 = model_Opt_Q_A2_1[i,1], 
-                                     sigma2 = model_Opt_Q_A2_1[i,2], 
-                                     h1 = model_Opt_Q_A2_1[i,0], 
+    data_GN_Q_A2_1.append(EMf_2Lay_HVP_Q(lambd, 
+                                     sigma1 = model_GN_Q_A2_1[i,1], 
+                                     sigma2 = model_GN_Q_A2_1[i,2], 
+                                     h1 = model_GN_Q_A2_1[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt)) 
     
-    data_Opt_Q_A2_2.append(EMf_2Lay_HVP_Q(lambd, 
-                                     sigma1 = model_Opt_Q_A2_2[i,1], 
-                                     sigma2 = model_Opt_Q_A2_2[i,2], 
-                                     h1 = model_Opt_Q_A2_2[i,0], 
+    data_GN_Q_A2_2.append(EMf_2Lay_HVP_Q(lambd, 
+                                     sigma1 = model_GN_Q_A2_2[i,1], 
+                                     sigma2 = model_GN_Q_A2_2[i,2], 
+                                     h1 = model_GN_Q_A2_2[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt)) 
     
-    data_Opt_Q_A2_3.append(EMf_2Lay_HVP_Q(lambd, 
-                                     sigma1 = model_Opt_Q_A2_3[i,1], 
-                                     sigma2 = model_Opt_Q_A2_3[i,2], 
-                                     h1 = model_Opt_Q_A2_3[i,0], 
+    data_GN_Q_A2_3.append(EMf_2Lay_HVP_Q(lambd, 
+                                     sigma1 = model_GN_Q_A2_3[i,1], 
+                                     sigma2 = model_GN_Q_A2_3[i,2], 
+                                     h1 = model_GN_Q_A2_3[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt))
 
-    data_Opt_Q_A2_4.append(EMf_2Lay_HVP_Q(lambd, 
-                                     sigma1 = model_Opt_Q_A2_4[i,1], 
-                                     sigma2 = model_Opt_Q_A2_4[i,2], 
-                                     h1 = model_Opt_Q_A2_4[i,0], 
+    data_GN_Q_A2_4.append(EMf_2Lay_HVP_Q(lambd, 
+                                     sigma1 = model_GN_Q_A2_4[i,1], 
+                                     sigma2 = model_GN_Q_A2_4[i,2], 
+                                     h1 = model_GN_Q_A2_4[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt))
-    
+
+# Gauss-Newton IP
 for i in range(npos):
-    data_Opt_IP_A2_1.append(EMf_2Lay_HVP_IP(lambd, 
-                                     sigma1 = model_Opt_IP_A2_1[i,1], 
-                                     sigma2 = model_Opt_IP_A2_1[i,2], 
-                                     h1 = model_Opt_IP_A2_1[i,0], 
+    data_GN_IP_A2_1.append(EMf_2Lay_HVP_IP(lambd, 
+                                     sigma1 = model_GN_IP_A2_1[i,1], 
+                                     sigma2 = model_GN_IP_A2_1[i,2], 
+                                     h1 = model_GN_IP_A2_1[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt)) 
     
-    data_Opt_IP_A2_2.append(EMf_2Lay_HVP_IP(lambd, 
-                                     sigma1 = model_Opt_IP_A2_2[i,1], 
-                                     sigma2 = model_Opt_IP_A2_2[i,2], 
-                                     h1 = model_Opt_IP_A2_2[i,0], 
+    data_GN_IP_A2_2.append(EMf_2Lay_HVP_IP(lambd, 
+                                     sigma1 = model_GN_IP_A2_2[i,1], 
+                                     sigma2 = model_GN_IP_A2_2[i,2], 
+                                     h1 = model_GN_IP_A2_2[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt)) 
     
-    data_Opt_IP_A2_3.append(EMf_2Lay_HVP_IP(lambd, 
-                                     sigma1 = model_Opt_IP_A2_3[i,1], 
-                                     sigma2 = model_Opt_IP_A2_3[i,2], 
-                                     h1 = model_Opt_IP_A2_3[i,0], 
+    data_GN_IP_A2_3.append(EMf_2Lay_HVP_IP(lambd, 
+                                     sigma1 = model_GN_IP_A2_3[i,1], 
+                                     sigma2 = model_GN_IP_A2_3[i,2], 
+                                     h1 = model_GN_IP_A2_3[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt))
 
-    data_Opt_IP_A2_4.append(EMf_2Lay_HVP_IP(lambd, 
-                                     sigma1 = model_Opt_IP_A2_4[i,1], 
-                                     sigma2 = model_Opt_IP_A2_4[i,2], 
-                                     h1 = model_Opt_IP_A2_4[i,0], 
+    data_GN_IP_A2_4.append(EMf_2Lay_HVP_IP(lambd, 
+                                     sigma1 = model_GN_IP_A2_4[i,1], 
+                                     sigma2 = model_GN_IP_A2_4[i,2], 
+                                     h1 = model_GN_IP_A2_4[i,0], 
                                      height = height,
                                      offsets = offsets,
                                      freq = freq,
                                      filt = filt))
 
 
-# Store data and model
+# Store simulated data
 np.save('data/data_GS_A2_1', data_GS_A2_1)
 np.save('data/data_GS_A2_2', data_GS_A2_2)
 np.save('data/data_GS_A2_3', data_GS_A2_3)
@@ -328,17 +343,17 @@ np.save('data/data_GS_IP_A2_2', data_GS_IP_A2_2)
 np.save('data/data_GS_IP_A2_3', data_GS_IP_A2_3)
 np.save('data/data_GS_IP_A2_4', data_GS_IP_A2_4)
 
-np.save('data/data_Opt_A2_1', data_Opt_A2_1)
-np.save('data/data_Opt_A2_2', data_Opt_A2_2)
-np.save('data/data_Opt_A2_3', data_Opt_A2_3)
-np.save('data/data_Opt_A2_4', data_Opt_A2_4)
+np.save('data/data_GN_A2_1', data_GN_A2_1)
+np.save('data/data_GN_A2_2', data_GN_A2_2)
+np.save('data/data_GN_A2_3', data_GN_A2_3)
+np.save('data/data_GN_A2_4', data_GN_A2_4)
 
-np.save('data/data_Opt_Q_A2_1', data_Opt_Q_A2_1)
-np.save('data/data_Opt_Q_A2_2', data_Opt_Q_A2_2)
-np.save('data/data_Opt_Q_A2_3', data_Opt_Q_A2_3)
-np.save('data/data_Opt_Q_A2_4', data_Opt_Q_A2_4)
+np.save('data/data_GN_Q_A2_1', data_GN_Q_A2_1)
+np.save('data/data_GN_Q_A2_2', data_GN_Q_A2_2)
+np.save('data/data_GN_Q_A2_3', data_GN_Q_A2_3)
+np.save('data/data_GN_Q_A2_4', data_GN_Q_A2_4)
 
-np.save('data/data_Opt_IP_A2_1', data_Opt_IP_A2_1)
-np.save('data/data_Opt_IP_A2_2', data_Opt_IP_A2_2)
-np.save('data/data_Opt_IP_A2_3', data_Opt_IP_A2_3)
-np.save('data/data_Opt_IP_A2_4', data_Opt_IP_A2_4)
+np.save('data/data_GN_IP_A2_1', data_GN_IP_A2_1)
+np.save('data/data_GN_IP_A2_2', data_GN_IP_A2_2)
+np.save('data/data_GN_IP_A2_3', data_GN_IP_A2_3)
+np.save('data/data_GN_IP_A2_4', data_GN_IP_A2_4)
