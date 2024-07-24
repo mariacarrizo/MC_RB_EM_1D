e1c5ce7d5541058d43da0e5a296b7a1ed0c0f028
Maria <carrizomascarell@terrasysgeo.com>
2024-07-24 11:18:33 +0200

    renamed

--- Field_Case/3_GaussNewton.py
+++ Field_Case/3_GaussNewton.py
@@ -1,4 +1,13 @@
-# Script that performs gradient based inversion for Field case
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" 
+Script Name: 3_GaussNewton.py
+Description: Script to perform Gauss-Newton inversion for Field case
+Author: @mariacarrizo
+Email: m.e.carrizomascarell@tudelft.nl
+Date created: 17/12/2023
+"""
+
 
 # Import libraries
 import pygimli as pg
@@ -8,7 +17,7 @@ import sys
 sys.path.insert(1, '../src')
 
 # Load forward modelling class for 2-layered models in field case
-from EM1D import EMf_2Lay_Opt_HV_field
+from EM1D import EMf_2Lay_GN_HV_field
 
 # Import the conductivities and thicknesses used to create the LU table
 conds = np.load('data/conds.npy')
@@ -34,16 +43,23 @@ freq = survey['freq']
 lambd = survey['lambd']
 filt = survey['filt']
 
+# Define initial model [h_1, sigma_1, sigma_2] (sigmas in S/m)
 m0 = [3, 100/1000, 100/1000]
+
+# Define regularization parameter (alpha in Equation 5)
 lam = 0
 
+# relative error for inversion class
+error = 1e-3 
+
+# Defining inversion limits and transformations
 transThk = pg.trans.TransLogLU(np.min(thick), np.max(thick))
 transSig = pg.trans.TransLogLU(np.min(conds), np.max(conds))
 
 # Optimization Q + IP
 
 # Initialize the forward modelling class
-EMf = EMf_2Lay_Opt_HV_field(lambd, height, offsets, freq, filt, nlay=2)
+EMf = EMf_2Lay_GN_HV_field(lambd, height, offsets, freq, filt, nlay=2)
 
 # Define transformation
 EMf.region(0).setTransModel(transThk)
@@ -54,15 +70,15 @@ invEM = pg.Inversion()
 invEM.setForwardOperator(EMf)
 
 # Relative error array
-error = 1e-3 # relative error 1e-3 
+
 relativeError = np.ones_like(data[0]) * error
-model_est = np.zeros((npos, nlay+1))
+model_GN = np.zeros((npos, nlay+1))
 
 # Start inversion
 # Perform inversion for each 1D model per position 
 for pos in range(npos):
     dataE = data[pos].copy()
-    model_est[pos] = invEM.run(dataE, relativeError, startModel= m0, lam=lam, verbose=False)
+    model_GN[pos] = invEM.run(dataE, relativeError, startModel= m0, lam=lam, verbose=False)
 
 # Save estimated model
-np.save('results/model_2Lay_Opt_field', model_est)
+np.save('results/model_2Lay_GN_field', model_GN)
